5d9ddcb0e49b555a84b5965c9771f4f1a6de6b2c
Ezequiel Amin <ezequielamin@outlook.com>
2023-08-31 15:07:49 -0300

    se agregó logica de inicio de sesion

---
+++ js/login/User.js
@@ -0,0 +1,14 @@
+export class User {
+    constructor(usuario,contraseña,email){
+        this.usuario = usuario;
+        this.contraseña = contraseña;
+        this.email = email;
+    }
+}
+
+export class UserWithoutPassword {
+    constructor(usuario,email){
+        this.usuario = usuario;
+        this.email = email;
+    }
+}
