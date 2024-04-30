c441fcb4b84c4020d405506409708ace5c99c197
Ezequiel Amin <ezequielamin@outlook.com>
2024-04-30 16:14:11 -0300

    creo la primera validacion

---
+++ js/admin/app.js
@@ -0,0 +1,29 @@
+// ---------------------------------
+// 1. Seleccionar elementos
+// ---------------------------------
+
+const $form = document.getElementById('form-contacto');
+const $inputNombre = document.getElementById('input-nombre');
+const $inputNumero = document.getElementById('input-numero');
+const $inputEmail = document.getElementById('input-email');
+const $inputImagen = document.getElementById('input-imagen');
+const $inputNotas = document.getElementById('input-notas');
+
+// ---------------------------------
+// 2. Event listener del submit
+// ---------------------------------
+
+$form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const nombre = $inputNombre.value;
+  const numero = $inputNumero.value;
+  const email = $inputEmail.value;
+  const imagen = $inputImagen.value;
+  const notas = $inputNotas.value;
+
+  if (!nombre || !numero || !email || !imagen) {
+    alert('No ingresó alguno de los campos requeridos');
+    return;
+  }
+});
---
+++ js/admin/validators.js
@@ -0,0 +1,28 @@
+// Retorna un booleano, siendo true cuando el campo está correcto
+export const validateName = (field) => {
+  // No sea vacio, null, etcs
+  if (!field || !field.value.trim()) {
+    field.classList.add('is-invalid');
+    field.classList.remove('is-valid');
+    return false;
+  }
+
+  // Longitud
+  if (field.value.trim().length < 3 || field.value.trim().length > 25) {
+    field.classList.add('is-invalid');
+    field.classList.remove('is-valid');
+    return false;
+  }
+
+  // Letras
+  const regex = /^[a-zA-ZÁÉÍÓÚáéíóúÜüÑñ\s]+$/;
+  if (!regex.test(field.value)) {
+    field.classList.add('is-invalid');
+    field.classList.remove('is-valid');
+    return false;
+  }
+
+  field.classList.remove('is-invalid');
+  field.classList.add('is-valid');
+  return true;
+};
