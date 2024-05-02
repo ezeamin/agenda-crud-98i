3f7fbbb1103ecfa25a221fef56fea8bb3b657463
Ezequiel Amin <ezequielamin@outlook.com>
2024-05-02 16:11:37 -0300

    muevo agregar contacto a su propia funcion segun comentado en #1

--- js/admin/abm.js
+++ js/admin/abm.js
@@ -1,17 +1,10 @@
-import { obtenerContactosDeLS } from '../utils.js';
 import { Contacto } from './Contacto.js';
+import { agregarContactoALS } from './utils.js';
 
 export const agregarContacto = (nombre, numero, email, imagen, notas) => {
   const contacto = new Contacto(nombre, numero, email, imagen, notas);
 
-  // 1. Traemos desde LS lo que haya guardado
-  const contactos = obtenerContactosDeLS();
-
-  // 2. Agregamos a lo que estaba guardado, lo nuevo
-  contactos.push(contacto);
-
-  // 3. Actualizamos los contactos en LS con los valores nuevos
-  localStorage.setItem('contactos', JSON.stringify(contactos));
+  agregarContactoALS(contacto);
 };
 
 export const editarContacto = () => {};
