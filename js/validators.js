5ff34d7324af6da266911b85b9a275fd65eefbe7
Ezequiel Amin <ezequielamin@outlook.com>
2024-05-14 15:02:39 -0300

    agrego logica del login

--- js/validators.js
+++ js/validators.js
@@ -50,7 +50,7 @@ export const validateNumber = (field) => {
 };
 
 // Retorna un booleano, siendo true cuando el campo está correcto
-export const validateEmail = ($field) => {
+export const validateEmail = ($field, showValidTick = true) => {
   // No sea vacio, null, etcs
   if (!$field || !$field.value.trim()) {
     $field.classList.add('is-invalid');
@@ -74,7 +74,7 @@ export const validateEmail = ($field) => {
   }
 
   $field.classList.remove('is-invalid');
-  $field.classList.add('is-valid');
+  if(showValidTick) $field.classList.add('is-valid');
   return true;
 };
 
@@ -105,5 +105,30 @@ export const validateUrl = ($field) => {
 
   $field.classList.remove('is-invalid');
   $field.classList.add('is-valid');
+  return true;
+};
+
+export const validatePassword = ($field) => {
+  // No sea vacio, null, etcs
+  if (!$field || !$field.value.trim()) {
+    $field.classList.add('is-invalid');
+    return false;
+  }
+
+  // Longitud
+  // if ($field.value.trim().length < 8 || $field.value.trim().length > 30) {
+  //   $field.classList.add('is-invalid');
+  //   return false;
+  // }
+
+  // Letras
+  // Al menos un caracter y un número, long minima de 8
+  // const regex = /^(?=.*[0-9])(?=.*[a-zA-Z]).{8,}$/;
+  // if (!regex.test($field.value)) {
+  //   $field.classList.add('is-invalid');
+  //   return false;
+  // }
+
+  $field.classList.remove('is-invalid');
   return true;
 };
